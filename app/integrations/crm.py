75e32762f0a806c9d3e34fb4997a58d2b995f2f8
Yugen Fring <g5759702@gmail.com>
2025-10-29 18:11:35 +0800

    add: 对文件名进行转换

--- app/integrations/crm.py
+++ app/integrations/crm.py
@@ -3,6 +3,17 @@ import os
 
 DOWNLOAD_URL = os.getenv('DOWNLOAD_URL')
 
+
+def sanitize_filename(filename):
+    """
+    将文件名中的特殊字符转换为安全字符
+    规则：
+    - 冒号 ':' → 下划线 '_'
+    - 空格 ' ' → 加号 '+'
+    """
+    result = filename.replace(':', '_').replace(' ', '+')
+    return result
+
 def add_sale_clue_crm(company_name, describe, phone_number, province, user_name, title):
     url = "http://172.30.3.80:32110/virtual/addSaleClue"
     
@@ -20,7 +31,7 @@ def add_sale_clue_crm(company_name, describe, phone_number, province, user_name,
         "email": "",
         "files": [
             {
-                "attachIndex": DOWNLOAD_URL + title + ".pdf",
+                "attachIndex": DOWNLOAD_URL + sanitize_filename(title) + ".pdf",
                 "ext": "pdf",
                 "filename": title
             }
